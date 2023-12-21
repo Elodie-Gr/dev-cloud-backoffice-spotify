d6d8c52bb339a24a4ad2582a9e5943a8694064bd
Fairouz BEN FRAJ <fairouz.benfraj@ynov.com>
2023-12-21 15:25:46 +0100

    update album cover

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,22 +5,10 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
 import Typography from '@mui/material/Typography';
-import {styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 
-// Update the CoverImage component
-const CoverImage = styled('img')({
-  width: 100,
-  height: 100,
-  objectFit: 'cover',
-  overflow: 'hidden',
-  flexShrink: 0,
-  borderRadius: 8,
-  backgroundColor: 'rgba(0,0,0,0.08)',
-  backgroundSize: 'cover',
-});
-
 export const Item = ({artistName, title, albumTitle, albumCover, album}) => {
+  const coverUrl = `http://localhost:5000/${albumCover}`;
   return (
     <ListItem
       alignItems="flex-start"
@@ -34,8 +22,12 @@ export const Item = ({artistName, title, albumTitle, albumCover, album}) => {
           </IconButton>
         </React.Fragment>
       }>
+      <img
+        src={coverUrl}
+        alt={`${artistName} - ${title}`}
+        style={{width: 64, height: 64, marginRight: 16}}
+      />
       <Box sx={{display: 'flex', alignItems: 'center'}}>
-        <CoverImage src={albumCover} />
         <Box sx={{ml: 1.5, minWidth: 0}}>
           <Typography variant="caption" color="text.secondary" fontWeight={500}>
             {artistName}
